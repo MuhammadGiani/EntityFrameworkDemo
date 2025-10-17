d4615418d4ad2ae4548d701bb501a03c052827a6
Muhammad Mustafa Giani <bscs2312161@szabist.pk>
2025-10-17 20:09:57 +0500

    Reconnected EntityFrameworkDemo project

---
+++ week_5/Infrastructure/DatabaseContext.cs
@@ -0,0 +1,21 @@
+﻿using EntityFrameWork.Infrastructure.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameWork.Infrastructure
+{
+                                 //base class to call its contructor use base keyword
+    public class DatabaseContext : DbContext
+    {
+        //make constructror
+        public DatabaseContext(DbContextOptions<DatabaseContext>options) : base(options)
+        {
+        }
+
+        DbSet<Customer> customers { get; set; }
+    }
+}
---
+++ week_5/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EntityFrameWork.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+using var host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((context, services) => //we have to make sql server services
+{
+    //Add services to the container
+
+    services.AddDbContext<DatabaseContext>(options =>
+    options.UseSqlServer("Data Source=(local);Initial Catalog=TechNova-Ecommerce-B;Integrated Security=True;Encrypt=False")
+    ); //Connection String of database will be pasted.
+
+})
+.Build();
